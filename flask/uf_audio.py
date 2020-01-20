e2ff05bafa448720eee61efc243be679a0a3536f
Maciej Urbanski <rooter@kyberian.net>
2020-01-20 03:12:14 +0100

    flask audio

---
+++ flask/uf_audio.py
@@ -0,0 +1,27 @@
+from flask import Flask, Response, render_template_string
+
+app = Flask(__name__)
+
+
+@app.route("/audio")
+def audio():
+    def wav_generator(sample_rate=8000, seconds=300, samples=1024):
+        yield b"RIFF$\x00}\x00WAVEfmt \x10\x00\x00\x00\x01\x00\x01\x00@\x1f"
+        yield b"\x00\x00@\x1f\x00\x00\x01\x00\x08\x00data\x00\x00}\x00"
+        for i in range(0, sample_rate * seconds, samples):
+            yield bytes(
+                (t * ((t >> 9 | t >> 13) & 15)) & 129 for t in range(i, i + samples)
+            )
+
+    return Response(wav_generator(), mimetype="audio/x-wav;codec=pcm")
+
+
+@app.route("/")
+def index():
+    return render_template_string(
+        '<audio controls><source src="{{ url_for("audio") }}" />'
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, threaded=True)
